c3d9cbf535c4565031a3d706f92f4d336bae02ac
Richard Pascarelli <Rpavesi84@gmail.com>
2019-11-12 15:12:03 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  @title = "Won't Believe" || "Guess" || "Top" || "Secret"
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :title, format: { with: (/#{@title}/i) }
 end
